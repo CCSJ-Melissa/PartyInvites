7974fb7de3a2e9cda94a1537fc533552c57d8a56
Melissa Paradiso <meltheeclipsequeen@gmail.com>
2018-09-20 16:43:41 -0500

    bug fixes, syntax clean up in homecontroller and in myview

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -7,53 +7,30 @@ using System;
 using Microsoft.AspNetCore.Mvc;
 using PartyInvites.Models;
 
-namespace PartyInvites.Controllers {
+namespace PartyInvites.Controllers
+{
 
-    public class HomeController : Controller {
+    public class HomeController : Controller
+    {
 
 
-        public ViewResult Index() {
+        public ViewResult Index()
+        {
             int hour = DateTime.Now.Hour;
             ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
             return View("MyView");
 
-                 }
-            public ViewResult RsvpForm()
-                return ViewResult();
-        
+        }
 
-        }
-             }
-        }
-    
-        public IActionResult Index()
+        public ViewResult RsvpForm()
         {
             return View();
         }
+    }
+}
+   
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
 
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+       
     
 
