7b7f8bff2dbec53ee04fcd7703c68d0ae973c169
Melissa Paradiso <meltheeclipsequeen@gmail.com>
2018-10-04 17:00:13 -0500

    Fixed validations

--- PartyInvites/Models/GuestResponse.cs
+++ PartyInvites/Models/GuestResponse.cs
@@ -11,7 +11,7 @@ namespace PartyInvites.Models
         [Required(ErrorMessage = "Please neter your name")]
         public string Name { get; set; }
         [Required(ErrorMessage = "Please enter your email address")]
-        [RegularExpression(".+\\@+\\..+",  ErrorMessage = "Please enter a valid email adress")]
+        [RegularExpression(".+\\@.+\\..+",  ErrorMessage = "Please enter a valid email adress")]
         public string Email { get; set; }
         [Required(ErrorMessage = "Please enter your phone number")]
         public string Phone {get; set; }
